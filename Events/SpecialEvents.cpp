66bea937c05a58deba1fb344f99c53f8c1fb4c49
Slian <slian.mhamid@campus.technion.ac.il>
2025-07-14 01:20:17 +0300

    D

--- Events/SpecialEvents.cpp
+++
@@ -1,7 +0,0 @@
-#include "SpecialEvents.h"
-
-SpecialEvents::SpecialEvents(const string& name) : Event(name){}
-
-std::string SpecialEvents::getDescription() const{
-      return eventName;
-}
--- Players/Archer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Archer.h"
-#include "CharacterRole.h"
-
-Archer::Archer(const std::string& role) : CharacterRole(role) {}
-
-int Archer::getCoins() const {
-    return 20;
-}
