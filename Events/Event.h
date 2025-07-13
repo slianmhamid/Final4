504e8a0b575dea0b254a1b03b83bff1d61f81ca7
Slian <slian.mhamid@campus.technion.ac.il>
2025-07-13 23:34:29 +0300

    Add

--- Events/Event.h
+++ Events/Event.h
@@ -1,25 +1,24 @@
-
 #pragma once
-#include "../Players/Player.h"
 
+#include "../Players/Player.h"
+#include "characterRole.h"
 #include <string>
-using std::string;
 
 class Event {
 protected:
-    std::string eventName;
+      std::string eventName;
 public:
-    /**
-     * Gets the description of the event
-     *
-     * @return - the description of the event
-    */
-    virtual string getDescription() const = 0;
-    Event(const string &name):eventName(name){}
-    virtual ~Event() = default;
-    virtual int applyEffect(Player &player) = 0;
-    virtual string eventEffectResult(Player &player,int effect) const = 0;
-    string getEventName() const{
-        return eventName;
-    }
+      /**
+       * Gets the description of the event
+       *
+       * @return - the description of the event
+      */
+      virtual std::string getDescription() const = 0;
+      Event(const std::string &name):eventName(name){}
+      virtual ~Event() = default;
+      virtual int applyEffect(Player& player) = 0;////
+      virtual std::string eventEffectResult(Player& player,int effect) const = 0;
+      std::string getEventName() const{
+            return eventName;
+      }
 };
